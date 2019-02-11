350ac203a07de0c8693e51bee5ea83e7ec872494
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-11 01:43:28 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,3 +2,17 @@
 def begins_with_r(array)
   array.all? { |s| s.start_with?('r') }
   end
+
+def contain_a_return(array)
+  array.select { |word| favor.include?("a") == true }
+end
+
+def first_wa
+end
+
+def remove_non_strings
+end
+
+def count_elements
+end
+

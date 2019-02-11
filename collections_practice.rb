089548cacbea408bd7f3a015c5960762ae859537
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-11 02:05:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,7 +8,7 @@ def contain_a(array)
 end
 
 def first_wa(array)
-  array.select { |word| word.include?("a") == true }
+  array.detect { |word| word.include?("a") == true }
 end
 
 def remove_non_strings
